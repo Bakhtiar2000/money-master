b2679652ae32ffebc4bda53f0aec07b3e6fd7197
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-02-16 15:50:22 +0600

    starting js

---
+++ js/calculate-expanses.js
@@ -0,0 +1,22 @@
+document.getElementById('expanse-calculator').addEventListener('click', function(){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const  x= document.getElementById('income');
+// const  x= document.getElementById('food-expanses');
+// const  x= document.getElementById('rent-expanses');
+// const  x= document.getElementById('clothes-expanses');
+// const  x= document.getElementById('total-expanses');
+// const  x= document.getElementById('balance');
---
+++ js/calculate-savings.js
@@ -0,0 +1,16 @@
+document.getElementById('savings-calculator').addEventListener('click', function(){
+
+});
+
+
+
+
+
+
+
+
+
+
+// const  x= document.getElementById('savings');
+// const  x= document.getElementById('saving-amount');
+// const  x= document.getElementById('remaining-balance');
